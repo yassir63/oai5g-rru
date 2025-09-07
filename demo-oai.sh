0c9d9f9f977544e14aa4b258e6a3508f938e758e
root <root@sopnode-l1.inria.fr>
2025-09-07 22:48:54 +0200

    Changing R2LAB_REPO

--- demo-oai.sh
+++ demo-oai.sh
@@ -329,13 +329,13 @@ CN_DEFAULT_GW=""
 
 ################################ oai-gnb chart parameters ########################
 OAI5G_RAN="$OAI5G_CHARTS/oai-5g-ran"
-R2LAB_REPO="docker.io/r2labuser"
+#R2LAB_REPO="docker.io/r2labuser"
+R2LAB_REPO="ghcr.io/ziyad-mabrouk/openairinterface5g"
 MY_REPO="ghcr.io/ziyad-mabrouk/openairinterface5g"
-#MY_REPO="docker.io/r2labuser"
 #
 
-#RAN_TAG="2025.w34"
-RAN_TAG="develop-ubuntu"
+RAN_TAG="2025.w34"
+#RAN_TAG="develop-ubuntu"
 GNB_NAME="gNB-r2lab"
 
 # DU/CU SPLIT parameters
