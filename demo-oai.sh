b6228c6e6f5868ee950e2b4d67e5c6184c634d96
root <root@sopnode-l1.inria.fr>
2025-09-07 21:11:38 +0200

    Back to OLD MT_REPO

--- demo-oai.sh
+++ demo-oai.sh
@@ -330,7 +330,8 @@ CN_DEFAULT_GW=""
 ################################ oai-gnb chart parameters ########################
 OAI5G_RAN="$OAI5G_CHARTS/oai-5g-ran"
 R2LAB_REPO="docker.io/r2labuser"
-MY_REPO="docker.io/r2labuser"
+MY_REPO="ghcr.io/ziyad-mabrouk/openairinterface5g"
+#MY_REPO="docker.io/r2labuser"
 #
 
 #RAN_TAG="2025.w34"
