e99f115a0871056c111b3bf474985cf5af95683d
Yassir Amami <91877785+yassir63@users.noreply.github.com>
2025-11-18 14:39:36 +0100

    Update repository URLs in demo-oai.sh

--- demo-oai.sh
+++ demo-oai.sh
@@ -329,9 +329,9 @@ CN_DEFAULT_GW=""
 
 ################################ oai-gnb chart parameters ########################
 OAI5G_RAN="$OAI5G_CHARTS/oai-5g-ran"
-#R2LAB_REPO="docker.io/r2labuser"
-R2LAB_REPO="docker.io/oaisoftwarealliance"
-MY_REPO="docker.io/oaisoftwarealliance"
+R2LAB_REPO="docker.io/r2labuser"
+#R2LAB_REPO="docker.io/oaisoftwarealliance"
+MY_REPO="ghcr.io/ziyad-mabrouk/openairinterface5g"
 #
 
 #RAN_TAG="2025.w34"
